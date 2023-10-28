3617ab59ba85a0ee7b1cde4c7f3d56ba3e250b9a
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-28 15:27:21 +0200

    Add new category form

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
-const teaCategory = require('../models/teaCategory');
+const TeaCategory = require('../models/teaCategory');
 const tea = require('../models/tea');
 
 // Display list of all tea categories
 exports.index = asyncHandler(async (req, res) => {
-  const numberOfTeaCategories = await teaCategory.countDocuments().exec();
-  const allTeaCategories = await teaCategory.find().exec();
+  const numberOfTeaCategories = await TeaCategory.countDocuments().exec();
+  const allTeaCategories = await TeaCategory.find().exec();
   res.render('teaCategoryList', {
     title: 'Tea Categories',
     numberOfTeaCategories,
@@ -19,7 +20,7 @@ exports.categoryDetail = asyncHandler(async (req, res) => {
   const categoryID = req.params.id;
 
   // Get category info
-  const category = await teaCategory.findById(categoryID).exec();
+  const category = await TeaCategory.findById(categoryID).exec();
 
   // Find all teas of this category
   const teasOfThisCategory = await tea
@@ -31,3 +32,50 @@ exports.categoryDetail = asyncHandler(async (req, res) => {
     teasOfThisCategory,
   });
 });
+
+// Display form for new category
+exports.newCategory = asyncHandler(async (req, res) => {
+  const errors = null;
+  res.render('forms/newCategoryForm', { errors });
+});
+
+// Handle new category form submission
+exports.newCategoryPost = [
+  // Validate and sanitize
+  body('name', 'Category name must have at least one character')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('description', 'Please fill in a short description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    const category = new TeaCategory({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error message(s).
+      res.render('forms/newCategoryForm', {
+        category,
+        errors: errors.array(),
+      });
+    } else {
+      // Data was valid
+      const categoryExists = await TeaCategory.findOne({ name: req.body.name })
+        .collation({ locale: 'en', strength: 2 }) // case insensitive
+        .exec();
+      if (categoryExists) {
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -15,4 +15,10 @@ router.get('/category/:id', categoryController.categoryDetail);
 // GET request for one tea
 router.get('/tea/:id', teaController.teaDetail);
 
+// GET request for new category
+router.get('/new-category', categoryController.newCategory);
+
+// POST request for new category form
+router.post('/new-category', categoryController.newCategoryPost);
+
 module.exports = router;
